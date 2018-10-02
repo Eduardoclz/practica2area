73dd3a1ab33d8ce4a53a138342f7c1084b1e3574
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-02 08:19:40 -0700

    Agregar archivos de proyecto.

---
+++ PracticaArea/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaArea
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            panelConfiguracion.Children.Clear();
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Rectangulo
+                    panelConfiguracion.Children.Add(
+                        new ControlAreaRectangulo());
+                    break;
+                case 1: //Triangulo
+                    panelConfiguracion.Children.Add(
+                        new ControlAreaTriangulo());
+                    break;
+                case 2: //Circulo
+                    panelConfiguracion.Children.Add(
+                        new ControlAreaCirculo());
+                    break;
+                case 3: //Trapecio
+                    panelConfiguracion.Children.Add(
+                        new ControlAreaTrapecio());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Rectangulo
+                    var controlAreaRectangulo =
+                        (ControlAreaRectangulo)
+                        panelConfiguracion.Children[0];
+
+                    float BaseRec =
+                        float.Parse(
+                        controlAreaRectangulo.txtBaseRec.Text);
+                    float AlturaRec =
+                        float.Parse(
+                        controlAreaRectangulo.txtAlturaRec.Text);
+              
+                    area = BaseRec * AlturaRec;
+                    break;
+                case 1: //Triangulo
+                    var controlAreaTriangulo =
+                       (ControlAreaTriangulo)
+                        panelConfiguracion.Children[0];
+                    float BaseTri =
+                        float.Parse(
+                           controlAreaTriangulo.txtBaseTri.Text);
+                    float AlturaTri =
+                        float.Parse(
+                           controlAreaTriangulo.txtAlturaTri.Text);
+                    area = (BaseTri * AlturaTri) / 2;
+
+                    break;
+                case 2: // Circulo
+                 var controlAreaCirculo =
+                        (ControlAreaCirculo)
+                       panelConfiguracion.Children[0];
+                    float radio =
+                       float.Parse(
+                            controlAreaCirculo.txtRadio.Text
+                            );
+                   area = 3.14f * radio * radio;
+                    break;
+                case 3: //Trapecio
+                    var controlAreaTrapecio =
+                        (ControlAreaTrapecio)
+                        panelConfiguracion.Children[0];
+
+                   float BaseMayor =
+                    float.Parse(controlAreaTrapecio.txtBaseMayor.Text);
+                    float BaseMenor =
+                   float.Parse(controlAreaTrapecio.txtBaseMenor.Text);
+                    float AlturaTra =
+                   float.Parse(controlAreaTrapecio.txtAltura.Text);
+                    area = ((BaseMayor + BaseMenor) *AlturaTra) / 2;
+
+
+               break;
+                default:
+                    break;
+            }
+
+            lblResultadoArea.Text =
+                area.ToString();
+        }
+    }
+}
